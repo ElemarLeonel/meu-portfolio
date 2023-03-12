98f1309bb7df9ce5a6312ebc4f31afa4d635ef3d
ElemarLeonel <elemarleonelbalduino@gmail.com>
2023-03-11 20:27:07 -0400

    Atualizando layout do portfólio

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -3,7 +3,7 @@ import { AnimationOnScroll } from "react-animation-on-scroll"
 
 export default function Projects() {
     return (
-        <section className="flex-col bg-gray-200 dark:bg-primary p-4" id="projects">
+        <section className="flex-col bg-secondary dark:bg-primary p-4" id="projects">
             <div className="mb-10 mt-10">
                 <AnimationOnScroll animateIn="animate__zoomIn">
                     <h1 className="text-primary text-center font-bold text-3xl
@@ -15,7 +15,7 @@ export default function Projects() {
             <div className="flex flex-row flex-wrap gap-5 justify-between mx-10 my-10">
                 {projects.map((project) => (
                     <div className="card w-96 sm:w-1/2 md:w-1/4 bg-accent 
-                    hover:bg-secondary shadow-xl" key={project.key}>
+                    hover:bg-gray-200 shadow-xl" key={project.key}>
                         <AnimationOnScroll animateIn="animate__fadeInUp">
                             <figure className="px-10 pt-10">
                                 <img src={project.image} alt={project.alt}
@@ -33,7 +33,7 @@ export default function Projects() {
                                 <div className="card-actions">
                                     <button className="btn btn-primary">
                                         <a href={project.link}
-                                            className="text-secondary">
+                                            className="text-secondary hover:text-accent">
                                             Acesse Agora
                                         </a>
                                     </button>
