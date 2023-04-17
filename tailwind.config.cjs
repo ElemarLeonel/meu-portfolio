1e4b92d92e5f2b971cc5e2fab09aa32ffeba6f81
ElemarLeonel <elemarleonelbalduino@gmail.com>
2023-04-16 22:10:14 -0400

    Atualizando CSS do modo light da página

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,7 +24,7 @@ module.exports = {
     fontFamily: {
       sans: ['Raleway', 'sans-serif'],
       cursive: ['Righteous', 'cursive']
-    }
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
